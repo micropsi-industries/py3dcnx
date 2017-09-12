4afc59dae6dd468821e26ade43d990b646b9954c
nemanja <nemanja@micropsi-industries.com>
2017-09-12 15:06:56 +0200

    Added setup.py OS dependent code

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
 from distutils.core import setup, Extension
+import sys
 
-module1 = Extension('py3dcnx',
-                    sources=['py3dcnx.c', 'py3dcnx_hidapi.c'])
+platform = sys.platform
+
+if 'linux' in platform:
+    module1 = Extension('py3dcnx', sources=['py3dcnx.c', 'py3dcnx_hidapi.c'],
+                        libraries=['hidapi-hidraw'])
+elif platform == 'darwin':
+    module1 = Extension('py3dcnx', sources=['py3dcnx.c', 'py3dcnx_hidapi.c'],
+                        libraries=['hidapi'])
+elif 'win' in platform:
+    module1 = Extension('py3dcnx',
+                        sources=['py3dcnx.c', 'py3dcnx_hidapi.c'],
+                        library_dirs=['/usr/local/lib'],
+                        libraries=['hidapi'])
+
 
 setup(name='Py3Dcnx',
       version='1.0',
