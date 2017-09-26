04757b6c83fcb6521ad42709ab0648ebd135936e
nemanja <nemanja@micropsi-industries.com>
2017-09-26 11:28:32 +0200

    Switching from Thread to Process, updating test and adding .gitignore

--- py3dcnx.py
+++ py3dcnx.py
@@ -1,5 +1,5 @@
 import _py3dcnx
-from threading import Thread
+from multiprocessing import Process
 
 event_types = ['button', 'rotate', 'translate']
 
@@ -8,9 +8,12 @@ class SpaceMouse:
     def __init__(self, num=0):
         self.handlers = dict()
         self.num = num
-        self.thread = Thread(target=self.event_loop_thread, args=(num,))
-        self.thread.start()
+        self.start()
         return
+
+    def start(self):
+        self.thread = Process(target=self.event_loop_thread, args=(self.num,))
+        self.thread.start()
 
     def event_loop_thread(self, num):
         while True:
@@ -21,12 +24,14 @@ class SpaceMouse:
             if event['type'] in self.handlers.keys():
                 self.handlers[event['type']](event)
 
-    def register_handler(self, handler, event='all'):
+    def register_handler(self, handler, event='all', data=None):
+        self.thread.terminate()
         if event is 'all':
             for e in event_types:
                 self.handlers[e] = handler
         elif event in event_types:
             self.handlers[event] = handler
+        self.start()
 
     def get_devices(self):
         return _py3dcnx.get_devices()
--- test.py
+++ test.py
@@ -3,19 +3,25 @@ from py3dcnx import SpaceMouse
 buttons = 0
 
 
-def button(event):
-    global buttons
-    new_buttons = event['val']
-    changes = new_buttons ^ buttons
-    if changes is not 0:
-        for i in range(2):
-            if changes & (1 << i):
-                if new_buttons & (1 << i):
-                    print('Button %d is down' % (i+1))
-                else:
-                    print('Button %d is up' % (i+1))
-    buttons = new_buttons
+class Listener():
+    def __init__(self):
+        self.name = 'ButtonListener'
+
+    def button(self, event):
+        global buttons
+        new_buttons = event['val']
+        changes = new_buttons ^ buttons
+        if changes is not 0:
+            for i in range(2):
+                if changes & (1 << i):
+                    print(self.name+':')
+                    if new_buttons & (1 << i):
+                        print('Button %d is down' % (i+1))
+                    else:
+                        print('Button %d is up' % (i+1))
+        buttons = new_buttons
 
 
+ls = Listener()
 sm = SpaceMouse()
-sm.register_handler(button, "button")
+sm.register_handler(ls.button, "button")
